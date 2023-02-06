057dc3c1a011749755f7dd43b31281d729489823
Benoit BRICE <benoit.brice@iddeos.fr>
2023-02-06 15:08:37 +0100

    Ajout des ApiResources pour eviter la reference circulaire

--- src/Entity/UserOpenHours.php
+++ src/Entity/UserOpenHours.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\UserOpenHoursRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserOpenHoursRepository::class)]
+#[ApiResource]
 class UserOpenHours
 {
     #[ORM\Id]
